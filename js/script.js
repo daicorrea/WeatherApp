b3330ada7d5df3b5a3d2da00ee45e0aed102a154
daiCorrea <daiane.ocorrea@gmail.com>
2017-05-29 17:44:14 -0300

    Adding Weather Icons

--- js/script.js
+++ js/script.js
@@ -3,11 +3,38 @@ $("document").ready(function() {
 
 });
 
+var weatherObj = {
+    "01d": "sunny",
+    "01n": "starry",
+    "02d": "rainbow",
+    "02n": "rainbow",
+    "03d": "cloudy",
+    "03n": "cloudy",
+    "04d": "cloudy",
+    "04n": "cloudy",
+    "09d": "rainy",
+    "09n": "rainy",
+    "10d": "rainy",
+    "10n": "rainy", 
+    "11d": "stormy",
+    "11n": "stormy",
+    "13d": "snowy",
+    "13n": "snowy",
+    "50d": "cloudy",
+    "50n": "cloudy"
+}
+
 function changeWeather(json) {
    console.log(json);
    var temp = Math.round(((json.main.temp) - 273.15));
-   console.log(temp);
-   $('.temperature').html(temp + ' &degC');
+   var local = json.name + '/' + json.sys.country;
+   var icon = '<div class="' + weatherObj[json.weather[0].icon] + '"></div>';
+   console.log(icon);
+   $('.temperature').html(temp + '&degC');
+   $('.local').html(local);
+   $('.icon-temp').html(icon);
+
+   document.body.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2013/10/02/23/03/dawn-190055_1280.jpg')";
 }
 
 function getCoords() {
